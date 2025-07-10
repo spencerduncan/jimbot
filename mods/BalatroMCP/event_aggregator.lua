5e261012cc6c4c98cbc5191ed659e881292a533d
Spencer Duncan <spencerjduncan@gmail.com>
2025-07-09 17:31:30 -0700

    Revert "Fix BalatroMCP event aggregation bugs"
    This reverts commit fe4dae8b965046d204cf237efc5291332312a65e.

--- mods/BalatroMCP/event_aggregator.lua
+++ mods/BalatroMCP/event_aggregator.lua
@@ -23,7 +23,7 @@ function EventAggregator:init(batch_window_ms)
     self.batch_window_ms = batch_window_ms or 100
     self.last_flush_time = love.timer.getTime() * 1000
     self.logger = BalatroMCP.components.logger
-    -- event_bus will be set after initialization in main.lua
+    self.event_bus = BalatroMCP.components.event_bus
 
     self.logger:info("Event aggregator initialized", {
         batch_window_ms = self.batch_window_ms,
